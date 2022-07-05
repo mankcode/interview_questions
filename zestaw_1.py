f962461f262a1a9045397c2e4be75722f60e0afe
Maciej Ankudowicz <kontakt@spaceproject.pl>
2022-07-05 14:21:42 +0200

    rozwiązane zadania

--- zestaw_1.py
+++ zestaw_1.py
@@ -10,11 +10,10 @@
 # 5. Returned list of postall codes
 
 
-def postal_code_generator(input_str1,input_str2):
-    return [(x[:2]+'-'+ x[2:]) for x in [str(element) for element in range(int("".join(input_str1[:2]+input_str1[3:])), int("".join(input_str2[:2]+input_str2[3:])))]]
+def postal_code_generator(input_str1, input_str2):
+    return [(x[:2]+'-' + x[2:]) for x in [str(element) for element in range(int("".join(input_str1[:2]+input_str1[3:])), int("".join(input_str2[:2]+input_str2[3:])))]]
 
-print(postal_code_generator("79-900","80-155"))
-
+# print(postal_code_generator("79-900", "80-155"))
 
 
 # ZADANIE 2. PODANA JEST LISTA ZAWIERAJĄCA ELEMENTY O WARTOŚCIACH 1-n. NAPISZ FUNKCJĘ KTÓRA SPRAWDZI JAKICH ELEMENTÓW BRAKUJE
@@ -27,33 +26,22 @@ print(postal_code_generator("79-900","80-155"))
 # 1. I generated list of all numbers 1-n without '0' and numbers from input
 # 2. I used this list for comprehension of results
 
-def missing_args(input,n):
+def missing_args(input, n):
     return [x for x in [x for x in range(n+1)][1:] if x not in input]
-    
 
-input = [2,3,7,4,9]
+
+input = [2, 3, 7, 4, 9]
 n = 10
-print(missing_args(input,n))
+# print(missing_args(input, n))
 
 
 # ZADANIE 3. NALEŻY WYGENEROWAĆ LISTĘ LICZB OD 2 DO 5.5 ZE SKOKIEM CO 0.5, DANE WYNIKOWE MUSZĄ BYĆ TYPU DECIMAL.
 # Wszystkie programy proszę zrealizować w postaci funkcji, bez interfejsu i walidacji danych.
 
 # Result
-# 1. I used numpy to import arange which provides working with floats
-# 2. Then I used Decimal class to represent all results as decimal values in list
-# I'm really curious if that's a point :)
 
-from numpy import arange as ar
-from decimal import Decimal
+def float_list_gen(start, stop):
+    return [(x / 2) for x in [x for x in range(int(start * 2), int((stop * 2) + 1))]]
 
-def float_list_generator(start,stop,step):    
-    return [Decimal(x) for x in ar(start, stop+0.5, step, dtype=float)]
-    
 
-start = 2
-stop = 5.5
-step = 0.5
-
-print(float_list_generator(start=start,stop=stop,step=step))
-
+# print(float_list_gen(2, 5.5))
